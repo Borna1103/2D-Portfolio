4c0a7faab420ae015eb4f93b9478683e30f27cec
Borna <Borna1103@msn.com>
2024-07-01 16:41:34 -0700

    changes

--- src/main.js
+++ src/main.js
@@ -1,17 +1,12 @@
-import { k } from "./kaboomCtx"
+import { k } from "./kaboomCtx";
 
-k.loadSprite("World-Tiles", "./World-Tiles.png") , {
+k.loadSprite("Character", "./Enemies.png" , {
     sliceX: 39,
     sliceY: 31,
     anims: {
-        "idle" : // add later
+        "idle-left" : {from: 28, to: 33, loop: true, speed: 8},
+        "idle-right" : {from: 28, to: 33, loop: true, speed: 8},
     }
-}
+});
 
-k.loadSprite("World-Tiles", "./World-Tiles.png") , {
-    sliceX: 39,
-    sliceY: 31,
-    anims: {
-        "idle" : // add later
-    }
-}
+k.loadSprite("map", "./map.png");
