d977d6dde12d78d1b361cb9997d9b9081a9a73ef
Borna <Borna1103@msn.com>
2024-09-20 14:39:46 -0700

    file path changes

--- src/sprites.js
+++ src/sprites.js
@@ -3,7 +3,7 @@ import { k } from "./kaboomCtx.js";
 
 
 // Map Sprite
-k.loadSprite("map",  "map.png");
+k.loadSprite("map",  "./map.png");
 
 // WASD Controlls
 // k.loadSprite("wKey", "./wasd.png", {
@@ -16,7 +16,7 @@ k.loadSprite("map",  "map.png");
 
 
 // Main Character Sprite
-k.loadSprite("player", "Enemies.png" , { 
+k.loadSprite("player", "./Enemies.png" , { 
     sliceX: 14,
     sliceY: 24,
     anims: {
